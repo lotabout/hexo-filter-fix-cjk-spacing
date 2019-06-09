f7670dbf6088170072000ef994bf33580e7c633d
Jinzhou Zhang <lotabout@gmail.com>
2019-06-09 09:01:47 +0800

    fix #3 post excerpt is not handled correctly
    The filter registered was `after_post_render` and the excerpt rendering
    also happens in this phase. Change to `before_post_render`.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fix_cjk_spacing = require('./lib/fix-cjk-spacing.js');
 // configuration
 var enabled = hexo.config.fix_cjk_spacing;
 
-hexo.extend.filter.register('after_post_render', function(data) {
+hexo.extend.filter.register('before_post_render', function(data) {
   if (enabled === undefined || enabled === true) {
     data.content = fix_cjk_spacing(data.content);
   }
